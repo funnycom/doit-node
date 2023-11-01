bf0e7650da1283a50635c8047b77468ee01ce7c0
Kyunghee Ko <funcom@gmail.com>
2023-11-01 13:25:35 +0900

    파일 이름 수정

--- basics/03/sol-2.js
+++ basics/03/sol-2.js
@@ -1,4 +1,7 @@
-const writeMyFile = require("./file-sol.js");
+// 현재 코드에서 사용하고 있는 writeData 함수를 내보내는 file.js 모듈을 작성하세요.
+// 여기에서는 파일 이름 중복을 피하기 위해 file.js 대신 file-sol.js를, mydata.txt 대신 mydata-sol.txt를 사용했습니다.
 
-const data = 'This is sample of mine';
-writeMyFile('./mydata-sol.txt', data);
+const writeData = require("./file-sol.js");
+
+const data = "This is sample of mine";
+writeData("./mydata-sol.txt", data);
